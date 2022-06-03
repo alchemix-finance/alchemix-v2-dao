a463dd30d62429d62f925cdec1ccad61f722b3fa
thetechnocratic <thetechnocratic@gmail.com>
2022-06-02 21:56:34 -0400

    add v2-foundry submodule, add events.

--- src/MemoProcessor.sol
+++ src/MemoProcessor.sol
@@ -2,13 +2,13 @@ pragma solidity 0.8.13;
 
 import "./interfaces/IMemoProcessor.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import "forge-std/console2.sol";
+import "../lib/v2-foundry/src/base/ErrorMessages.sol";
 
 contract MemoProcessor is IMemoProcessor, Ownable {
-    error Unauthorized();
-    error IllegalArgument();
-
+    /// @dev A mapping of registered sources that can send memos.
     mapping(address => bool) public sources;
+
+    /// @dev A mapping of memo signatures to lists of listeners.
     mapping(bytes4 => address[]) public listeners;
 
     constructor() Ownable() {
@@ -17,11 +17,12 @@ contract MemoProcessor is IMemoProcessor, Ownable {
 
     modifier onlySource() {
         if (!sources[msg.sender]) {
-            revert Unauthorized();
+            revert Unauthorized("MemoProcessor: only source");
         }
         _;
     }
 
+    /// @inheritdoc IMemoProcessor
     function processMemo(bytes calldata eventData) external onlySource {
         bytes4 memoSig;
         assembly {
@@ -30,32 +31,42 @@ contract MemoProcessor is IMemoProcessor, Ownable {
         for (uint256 i = 0; i < listeners[memoSig].length; i++) {
             listeners[memoSig][i].call(eventData);
         }
+        emit MemoProcessed(eventData);
     }
 
+    /// @inheritdoc IMemoProcessor
     function registerSource(address source) external onlyOwner {
         sources[source] = true;
+        emit SourceRegistered(source);
     }
 
+    /// @inheritdoc IMemoProcessor
     function deRegisterSource(address source) external onlyOwner {
         sources[source] = false;
+        emit SourceDeRegistered(source);
     }
 
+    /// @inheritdoc IMemoProcessor
     function registerListener(bytes4 memoSig, address listener) external onlyOwner {
         for (uint256 i = 0; i < listeners[memoSig].length; i++) {
             if (listeners[memoSig][i] == listener) {
-                revert IllegalArgument();
+                revert IllegalArgument("MemoProcessor: listener exists");
             }
         }
         listeners[memoSig].push(listener);
+        emit ListenerRegistered(memoSig, listener);
     }
     
+    /// @inheritdoc IMemoProcessor
     function deRegisterListener(bytes4 memoSig, address listener) external onlyOwner {
         for (uint256 i = 0; i < listeners[memoSig].length; i++) {
             if (listeners[memoSig][i] == listener) {
                 listeners[memoSig][i] = listeners[memoSig][listeners[memoSig].length - 1];
                 listeners[memoSig].pop();
-                break;
+                emit ListenerDeRegistered(memoSig, listener);
+                return;
             }
         }
+        revert IllegalArgument("MemoProcessor: listener does not exist");
     }
 }
--- src/interfaces/IMemoProcessor.sol
+++ src/interfaces/IMemoProcessor.sol
@@ -1,9 +1,55 @@
 pragma solidity 0.8.13;
 
 interface IMemoProcessor {
+    /// @dev Emitted when a memo is successfully processed.
+    event MemoProcessed(bytes memoData);
+
+    /// @dev Emitted when a source is registered.
+    event SourceRegistered(address source);
+
+    /// @dev Emitted when a source is de-registered.
+    event SourceDeRegistered(address source);
+
+    /// @dev Emitted when a listener is registered.
+    event ListenerRegistered(bytes4 memoSig, address listener);
+
+    /// @dev Emitted when a listener is de-registered.
+    event ListenerDeRegistered(bytes4 memoSig, address listener);
+
+    /// @dev Process a memo and send it to the registered listeners.
+    ///
+    /// @notice This function reverts if the caller is not a registered source.
+    ///
+    /// @param eventData    The packed bytes to be used to call each listener.
     function processMemo(bytes calldata eventData) external;
+
+    /// @dev Register a source so that it can send memos.
+    ///
+    /// @notice This function reverts if the caller is not the admin.
+    ///
+    /// @param source   The address of the source.
     function registerSource(address source) external;
+
+    /// @dev DeRegister a source so that it cannot send memos.
+    ///
+    /// @notice This function reverts if the caller is not the admin.
+    ///
+    /// @param source   The address of the source.
     function deRegisterSource(address source) external;
+
+    /// @dev Register a listener so that it can receive memos.
+    ///
+    /// @notice This function reverts if the caller is not the admin.
+    ///
+    /// @param memoSig  The function signature of the memo that the listener wants to receive.
+    /// @param listener The address of the listener.
     function registerListener(bytes4 memoSig, address listener) external;
+
+    /// @dev DeRegister a listener so that it can receive memos.
+    ///
+    /// @notice This function reverts if the caller is not the admin.
+    ///
+    /// @param memoSig  The function signature of the memo that the listener no longer wants to receive.
+    /// @param listener The address of the listener.
     function deRegisterListener(bytes4 memoSig, address listener) external;
 }
