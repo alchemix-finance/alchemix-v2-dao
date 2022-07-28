057a8977b6b135455a9d90940625494648122d05
toyvo <toyvo.mail@proton.me>
2022-07-28 12:06:35 -0500

    team to admin

--- src/interfaces/IGaugeFactory.sol
+++ src/interfaces/IGaugeFactory.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.15;
 
 interface IGaugeFactory {
-    function team() external view returns (address);
+    function admin() external view returns (address);
     function createGauge(address, address, address) external returns (address);
 }
