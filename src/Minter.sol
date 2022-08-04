4b06c70a4483e37472d81ea585438c2051bcf061
toyvo <toyvo.mail@proton.me>
2022-08-04 18:58:39 -0500

    emissions tests

--- src/Minter.sol
+++ src/Minter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.15;
 
+import "forge-std/console2.sol";
+
 import "./libraries/Math.sol";
 import "./interfaces/IRewardsDistributor.sol";
 import "./interfaces/IVelo.sol";
@@ -8,7 +10,7 @@ import "./interfaces/IVoter.sol";
 import "./interfaces/IVotingEscrow.sol";
 import "./interfaces/IAlchemicToken.sol";
 
-import { IERC20Mintable } from "./interfaces/IERC20Mintable.sol";
+import { IAlchemixToken } from "./interfaces/IAlchemixToken.sol";
 
 struct InitializationParams {
 	address voter; // the voting & distribution system
@@ -21,18 +23,19 @@ struct InitializationParams {
 }
 
 contract Minter {
-	uint256 internal constant WEEK = 86400 * 7; // allows minting once per week (reset every Thursday 00:00 UTC)
-	uint256 internal constant EMISSION = 990;
-	uint256 internal constant PRECISION = 1000;
-	uint256 internal constant TAIL = 2194 * 10e18;
-	uint256 internal constant BLOCKS_PER_WEEK = 45000 * 10e18;
+	// Allows minting once per epoch (epoch = 1 week, reset every Thursday 00:00 UTC)
+	uint256 internal constant EPOCH = 86400 * 7;
 
-	IERC20Mintable public alcx = IERC20Mintable(0xdBdb4d16EdA451D0503b854CF79D55697F90c8DF);
+	// Tail emissions rate
+	uint256 public constant tailEmissionsRate = 2194 * 10e18;
+
+	IAlchemixToken public alcx = IAlchemixToken(0xdBdb4d16EdA451D0503b854CF79D55697F90c8DF);
 	IVoter public immutable voter;
 	IVotingEscrow public immutable ve;
 	IRewardsDistributor public immutable rewardsDistributor;
 
-	uint256 public weekly = 15000000e18;
+	uint256 public epochEmissions;
+
 	uint256 public activePeriod;
 
 	address internal initializer;
@@ -43,7 +46,7 @@ contract Minter {
 	uint256 public rewards;
 	uint256 public supply;
 
-	event Mint(address indexed sender, uint256 weekly, uint256 circulatingSupply, uint256 circulatingEmission);
+	event Mint(address indexed sender, uint256 epoch, uint256 circulatingSupply, uint256 circulatingEmissions);
 
 	constructor(InitializationParams memory params) {
 		stepdown = params.stepdown;
@@ -53,9 +56,9 @@ contract Minter {
 		admin = msg.sender;
 		voter = IVoter(params.voter);
 		ve = IVotingEscrow(params.ve);
-		alcx = IERC20Mintable(params.alcx);
+		alcx = IAlchemixToken(params.alcx);
 		rewardsDistributor = IRewardsDistributor(params.rewardsDistributor);
-		activePeriod = ((block.timestamp + WEEK) / WEEK) * WEEK;
+		activePeriod = ((block.timestamp + EPOCH) / EPOCH) * EPOCH;
 	}
 
 	function initialize() external {
@@ -73,45 +76,47 @@ contract Minter {
 		admin = pendingAdmin;
 	}
 
-	// calculate circulating supply as total token supply - locked supply
-	function circulatingSupply() public view returns (uint256) {
+	// Circulating supply is total token supply - locked supply
+	function circulatingAlcxSupply() public view returns (uint256) {
 		return alcx.totalSupply() - ve.totalSupply();
 	}
 
-	function weeklyEmission() public view returns (uint256) {
+	// Amount of emission for the current epoch
+	function epochEmission() public view returns (uint256) {
 		return rewards - stepdown;
 	}
 
-	function weeklyEmissionRate() public view returns (uint256) {
+	// TBD if necessary
+	function epochEmissionRate() public view returns (uint256) {
 		return (rewards / (rewards + stepdown)) * 1e18 * 52e18;
 	}
 
-	function circulatingEmissions() public view returns (uint256) {
+	function circulatingEmissionsSupply() public view returns (uint256) {
 		return supply;
 	}
 
+	// TODO logic to distrubte minted tokens to veALCX holders
 	// calculate inflation and adjust ve balances accordingly
-	// this should adjust the balances of veALCX holders aka gALCX?
-	// think we should move the logic for veALCX distrubtion to the rewards distributor
 	// function veGrowth(uint256 _minted) public view returns (uint256) {
 	// 	uint256 _veTotal = ve.totalSupply();
 	// 	uint256 _alcxTotal = alcx.totalSupply();
 	// 	return (((((_minted * _veTotal) / _alcxTotal) * _veTotal) / _alcxTotal) * _veTotal) / _alcxTotal / 2;
 	// }
 
-	// update period can only be called once per cycle (1 week)
+	// update period can only be called once per epoch (1 week)
 	function updatePeriod() external returns (uint256) {
 		uint256 _period = activePeriod;
-		if (block.timestamp >= _period + WEEK && initializer == address(0)) {
-			// only trigger if new week
-			_period = (block.timestamp / WEEK) * WEEK;
-			activePeriod = _period;
-			weekly = weeklyEmission();
 
-			// uint256 _growth = veGrowth(weekly);
+		if (block.timestamp >= _period + EPOCH && initializer == address(0)) {
+			// only trigger if new epoch
+			_period = (block.timestamp / EPOCH) * EPOCH;
+			activePeriod = _period;
+			epochEmissions = epochEmission();
+
 			uint256 _balanceOf = alcx.balanceOf(address(this));
-			if (_balanceOf < weekly) {
-				alcx.mint(address(this), weekly - _balanceOf);
+
+			if (_balanceOf < epochEmissions) {
+				alcx.mint(address(this), epochEmissions - _balanceOf);
 			}
 
 			// Set rewards for next epoch
@@ -120,19 +125,20 @@ contract Minter {
 			// Adjust updated emissions total
 			supply += rewards;
 
-			// Once we reach the plateau stepdown becomes 0
-			if (rewards <= TAIL) {
+			// Once we reach the emissions tail stepdown is 0
+			if (rewards <= tailEmissionsRate) {
 				stepdown = 0;
 			}
 
+			// TODO logic to distrubte minted tokens to veALCX holders
 			// require(alcx.transfer(address(rewardsDistributor), _growth));
-			rewardsDistributor.checkpoint_token(); // checkpoint token balance that was just minted in rewards distributor
-			rewardsDistributor.checkpoint_total_supply(); // checkpoint supply
+			// rewardsDistributor.checkpoint_token(); // checkpoint token balance that was just minted in rewards distributor
+			// rewardsDistributor.checkpoint_total_supply(); // checkpoint supply
 
-			alcx.approve(address(voter), weekly);
-			voter.notifyRewardAmount(weekly);
+			// alcx.approve(address(voter), epoch);
+			// voter.notifyRewardAmount(epoch);
 
-			emit Mint(msg.sender, weekly, circulatingSupply(), circulatingEmissions());
+			emit Mint(msg.sender, epochEmissions, circulatingAlcxSupply(), circulatingEmissionsSupply());
 		}
 		return _period;
 	}
--- test/Minter.t.sol
+++ test/Minter.t.sol
@@ -2,37 +2,34 @@
 pragma solidity ^0.8.15;
 
 import "forge-std/console2.sol";
-import { DSTest } from "ds-test/test.sol";
 
 import { DSTestPlus } from "./utils/DSTestPlus.sol";
 import { Minter, InitializationParams } from "../src/Minter.sol";
-import { IERC20Mintable } from "../src/interfaces/IERC20Mintable.sol";
+import { IAlchemixToken } from "../src/interfaces/IAlchemixToken.sol";
 import { IVotingEscrow } from "../src/interfaces/IVotingEscrow.sol";
 import { IRewardsDistributor } from "../src/interfaces/IRewardsDistributor.sol";
 import { Voter } from "../src/Voter.sol";
-import { PairFactory } from "../src/factories/PairFactory.sol";
-import { GaugeFactory } from "../src/factories/GaugeFactory.sol";
-import { BribeFactory } from "../src/factories/BribeFactory.sol";
 
 contract MinterTest is DSTestPlus {
-	IERC20Mintable public alcx = IERC20Mintable(0xdBdb4d16EdA451D0503b854CF79D55697F90c8DF);
+	IAlchemixToken public alcx = IAlchemixToken(0xdBdb4d16EdA451D0503b854CF79D55697F90c8DF);
 	IVotingEscrow public veALCX = IVotingEscrow(address(alcx));
 	IRewardsDistributor public rewardsDistributor = IRewardsDistributor(address(veALCX));
-	Voter voter;
-	PairFactory pairFactory;
-	GaugeFactory gaugeFactory;
-	BribeFactory bribeFactory;
+	address constant admin = 0x8392F6669292fA56123F71949B52d883aE57e225;
+
+	// TODO add emissions distributions to tests with voter contract
+	Voter voter = Voter(address(0x000000000000000000000000000000000000dEaD));
+
 	Minter minter;
+
+	// Current emission values
 	uint256 supply = 1793678 * 10e18;
 	uint256 rewards = 12724 * 10e18;
 	uint256 stepdown = 130 * 10e18;
+	uint256 nextEpoch = 86400 * 14;
+	uint256 epochsUntilTail = 80;
+	uint256 supplyAtTail = 2392609 * 10e18;
 
-	function setup() public {
-		pairFactory = new PairFactory();
-		gaugeFactory = new GaugeFactory(address(pairFactory));
-		bribeFactory = new BribeFactory();
-		voter = new Voter(address(veALCX), address(gaugeFactory), address(bribeFactory));
-
+	function setUp() public {
 		InitializationParams memory params = InitializationParams(
 			address(voter),
 			address(veALCX),
@@ -44,9 +41,53 @@ contract MinterTest is DSTestPlus {
 		);
 
 		minter = new Minter(params);
+
+		// Give the minter role to the minter contract
+		hevm.startPrank(admin, admin);
+		alcx.grantRole(keccak256("MINTER"), address(minter));
+		hevm.stopPrank();
+
+		// Initialize minter
+		minter.initialize();
 	}
 
-	function testWeeklyEmissions() external {
-		console2.log("here");
+	// Test emissions for a single epoch
+	function testEpochEmissions() external {
+		// Set the block timestamp to be the next epoch
+		hevm.warp(block.timestamp + nextEpoch);
+
+		uint256 currentTotalEmissions = minter.circulatingEmissionsSupply();
+		uint256 epochEmissions = minter.epochEmission();
+
+		// Mint emissions for epoch
+		minter.updatePeriod();
+
+		uint256 totalAfterEpoch = minter.circulatingEmissionsSupply();
+		emit log_named_uint("emissions after one epoch (ether)", totalAfterEpoch / 10e18);
+
+		assertEq(totalAfterEpoch, currentTotalEmissions + epochEmissions);
+	}
+
+	// Test reaching emissions tail
+	function testTailEmissions() external {
+		// Mint emissions for the amount of epochs until tail emissions target
+		for (uint8 i = 0; i <= epochsUntilTail; ++i) {
+			hevm.warp(block.timestamp + nextEpoch);
+			minter.updatePeriod();
+		}
+
+		uint256 tailRewards = minter.rewards();
+		uint256 tailStepdown = minter.stepdown();
+		uint256 tailEmissionSupply = minter.circulatingEmissionsSupply();
+		emit log_named_uint("tail emissions supply (ether)", tailEmissionSupply / 10e18);
+
+		// Assert rewards are the constant tail emissions value
+		assertEq(tailRewards, minter.tailEmissionsRate());
+
+		// Assert stepdown is 0 once tail emissions are reached
+		assertEq(tailStepdown, 0);
+
+		// Assert total emissions are the approximate target at the tail
+		assertApproxEq(tailEmissionSupply, supplyAtTail, 17 * 10e18);
 	}
 }
