6f9fb5255695493db59c4fe657c5f9f69811c75d
toyvo <toyvo.mail@proton.me>
2022-08-22 21:27:10 -0400

    comment updates

--- src/interfaces/IRewardsDistributor.sol
+++ src/interfaces/IRewardsDistributor.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.15;
 
 interface IRewardsDistributor {
-    function checkpoint_token() external;
-    function checkpoint_total_supply() external;
+    function checkpointToken() external;
+
+    function checkpointTotalSupply() external;
 }
