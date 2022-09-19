8a70636392b846c36507acc9364ab656f668edd2
toyvo <toyvo.mail@proton.me>
2022-09-19 14:47:48 -0500

    test order

--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
-make test_all
+make test_file FILE=StakingGauge
+make test_file FILE=AlchemixGovernor
+make test_file FILE=Voting
+make test_file FILE=MemoProcessor
+make test_file FILE=Minter
+make test_file FILE=gALCX
+make test_file FILE=RewardsDistributor
+make test_file FILE=VotingEscrow
