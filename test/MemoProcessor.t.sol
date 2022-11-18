f7e18e0aa2683fa14288b676a99a30e826294f2b
thetechnocratic <thetechnocratic@gmail.com>
2022-11-17 20:47:58 -0500

    fix tests?

--- test/MemoProcessor.t.sol
+++ test/MemoProcessor.t.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
-
 import { MemoProcessor } from "src/MemoProcessor.sol";
 import { VotingEscrow } from "src/VotingEscrow.sol";
-
-import { DSTest } from "forge-std/test.sol";
 import { IERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
 import { DSTestPlus } from "./utils/DSTestPlus.sol";
 import { Hevm } from "./utils/Hevm.sol";
 
