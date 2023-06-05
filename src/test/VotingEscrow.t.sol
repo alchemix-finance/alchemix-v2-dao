949094be8699e2c6e5a49d6d4d9e60fb435dfb60
thetechnocratic <thetechnocratic@gmail.com>
2023-06-05 09:20:36 -0400

    another breaking test.

--- src/test/VotingEscrow.t.sol
+++ src/test/VotingEscrow.t.sol
@@ -716,4 +716,27 @@ contract VotingEscrowTest is BaseTest {
 
         assertGt(veALCX.balanceOfTokenAt(tokenId, block.timestamp - 2 days), veALCX.balanceOfTokenAt(tokenId,  block.timestamp - 1 days), "after final warp");
     }
+
+    function testManipulatePastBalanceWithDeposit() public {
+        uint256 tokenId = createVeAlcx(admin, TOKEN_1, MAXTIME, false);
+
+        minter.updatePeriod();
+        hevm.warp(block.timestamp + 4 days + 1);
+        hevm.roll(block.number + 4 days / 12);
+
+        uint256 t2Dp1 = block.timestamp - (2 days + 1);
+        uint256 t2Dm1 = block.timestamp - (2 days - 1);
+
+        uint256 bal2DaysPlus1 = veALCX.balanceOfTokenAt(tokenId, t2Dp1);
+        uint256 bal2DaysMinus1 = veALCX.balanceOfTokenAt(tokenId, t2Dm1);
+
+        assertGt(bal2DaysPlus1, bal2DaysMinus1, "first check");
+
+        deal(bpt, address(this), TOKEN_1);
+        IERC20(bpt).approve(address(veALCX), TOKEN_1);
+        veALCX.depositFor(tokenId, TOKEN_1);
+
+        assertEq(veALCX.balanceOfTokenAt(tokenId, t2Dp1), bal2DaysPlus1, "after deposit, 2 days + 1");
+        assertEq(veALCX.balanceOfTokenAt(tokenId, t2Dm1), bal2DaysMinus1, "after deposit, 2 days - 1");
+    }
 }
