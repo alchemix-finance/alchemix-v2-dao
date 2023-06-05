d758abe4b136e2b6d19316b0309aa429c4b9fbc6
thetechnocratic <thetechnocratic@gmail.com>
2023-06-05 09:36:13 -0400

    fixed, lets see if it breaks anything else.

--- src/VotingEscrow.sol
+++ src/VotingEscrow.sol
@@ -1664,6 +1664,7 @@ contract VotingEscrow is IERC721, IERC721Metadata, IVotes {
             return 0;
         } else {
             Point memory lastPoint = userPointHistory[_tokenId][_epoch];
+            if (_time < lastPoint.ts) lastPoint = userPointHistory[_tokenId][_epoch - 1];
 
             // If max lock is enabled bias is unchanged
             int256 biasCalculation = locked[_tokenId].maxLockEnabled
