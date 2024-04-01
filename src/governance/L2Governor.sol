c3ac67d78af2a49f7a00a670090691b48ab44f22
toyvo <toyvo.mail@proton.me>
2024-04-01 15:26:56 -0400

    resolves 5.12

--- src/governance/L2Governor.sol
+++ src/governance/L2Governor.sol
@@ -602,7 +602,7 @@ abstract contract L2Governor is Context, ERC165, EIP712, IGovernor, IERC721Recei
     /**
      * @dev Relays a transaction or function call to an arbitrary target. In cases where the governance executor
      * is some contract other than the governor itself, like when using a timelock, this function can be invoked
-     * in a governance proposal to recover tokens or Ether that was sent to the governor contract by mistake.
+     * in a governance proposal.
      * Note that if the executor is simply the governor itself, use of `relay` is redundant.
      */
     function relay(address target, uint256 value, bytes calldata data) external virtual onlyGovernance {
